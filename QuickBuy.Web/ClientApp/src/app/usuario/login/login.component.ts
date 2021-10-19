b8859f35d7f6bcc350d39d7af18d3bd73e1c7f54
Weksslei <weksslei@live.com>
2021-10-18 21:14:02 -0300

    Correção no código

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,13 +1,11 @@
-import { Component, NgModule, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
-import { CommonModule } from "@angular/common";
 import { Router, ActivatedRoute } from "@angular/router";
 import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
-
 @Component({
-    selector: "app-login",
-    templateUrl: "./login.component.html",
-    styleUrls: ["./login.component.css"]
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 //@NgModule({
 //  declarations: [
@@ -17,51 +15,33 @@ import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
 //  exports: [
 //    LoginComponent],
 //  providers:[]})
-
 export class LoginComponent implements OnInit {
   public usuario;
   public returnUrl: string;
   public mensagem: string;
-  public ativar_spinner:  boolean;
-
+  public ativar_spinner: boolean;
   constructor(private router: Router, private activatedRouter: ActivatedRoute, private usuarioServico: UsuarioServico) {
-
   }
   ngOnInit(): void {
     this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
     this.usuario = new Usuario();
-    }
+  }
   entrar() {
     this.ativar_spinner = true;
     this.usuarioServico.verificarUsuario(this.usuario)
-      .subscribe(
-        usuario_json => {
-          // essa linha será executada no caso de retorno sem erros
-          this.usuarioServico.usuario = usuario_json;
-
-          if (this.returnUrl == null) {
-            this.router.navigate(['/']);
-          } else {
-            this.router.navigate([this.returnUrl]);
-          }
-        },
-        err => {
-          console.log(err.error);
-          this.mensagem = err.error;
-          this.ativar_spinner = false;
-
+      .subscribe(usuario_json => {
+        // essa linha será executada no caso de retorno sem erros
+        this.usuarioServico.usuario = usuario_json;
+        if (this.returnUrl == null) {
+          this.router.navigate(['/']);
         }
-      );
-
-
-
-    //if (this.usuario.email == "weksslei@live.com" && this.usuario.senha == "250980") {
-    //  sessionStorage.setItem("usuario-autenticado", "1");
-    //  this.router.navigate([this.returnUrl]);
-      
-    // }
-
-//alert(this.usuario.email +"-"+ this.usuario.senha);
+        else {
+          this.router.navigate([this.returnUrl]);
+        }
+      }, err => {
+        console.log(err.error);
+        this.mensagem = err.error;
+        this.ativar_spinner = false;
+      });
   }
 }
-
